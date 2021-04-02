81584149afd7f9e54f68af8f1fc62fcb03994653
theblackangelx <markzoutendijk@live.nl>
2021-04-02 22:07:37 +0200

    Initial commit

---
+++ src/Mechanic.js
@@ -0,0 +1,14 @@
+exports.mod = () => {
+
+    logger.logInfo("[mod] NoRepairPenalty by TheBlackAngelx");
+    let config = require("../config.json");
+    let quality = config.gameplay.repairPenaltyMechanic;
+    
+    if (config.gameplay.repairPenaltyMechanic != false) {
+        
+        data = fileIO.readParsed(db.cacheBase.traders["5a7c2eca46aef81a7ca2145d"].base)
+        data.repair.quality = quality
+        fileIO.write(db.cacheBase.traders["5a7c2eca46aef81a7ca2145d"].base, data)
+        logger.logSuccess("[MOD NRP] Mechanic repair penalty set to " + quality);
+    }
+}
