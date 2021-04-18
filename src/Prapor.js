96383fd7c97787f677e15a368e587fde7f9b82d4
theblackangelx <50122147+theblackangelx@users.noreply.github.com>
2021-04-18 23:24:45 +0200

    Update Prapor.js

--- src/Prapor.js
+++ src/Prapor.js
@@ -3,6 +3,7 @@ exports.mod = () => {
     logger.logInfo("[mod] NoRepairPenalty by TheBlackAngelx");
     let config = require("../config.json");
     let quality = config.gameplay.repairPenaltyPrapor;
+    
     if (config.gameplay.repairPenaltyPrapor != false) {
         
         data = fileIO.readParsed(db.cacheBase.traders["54cb50c76803fa8b248b4571"].base)
